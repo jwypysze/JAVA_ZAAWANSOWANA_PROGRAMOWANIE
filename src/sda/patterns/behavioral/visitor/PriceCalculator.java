c2cea76ef09334f68df14a42c75626efddcff5d8
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2023-02-07 20:55:29 +0100

    Wzorce behawioralne - visitor

---
+++ src/sda/patterns/behavioral/visitor/PriceCalculator.java
@@ -0,0 +1,27 @@
+package sda.patterns.behavioral.visitor;
+
+public class PriceCalculator implements Calculator {
+
+    @Override
+    public int calculate(Butter butter) {
+        System.out.println(butter.getName() + " price: " + butter.getCommonValue());
+        return butter.getCommonValue();
+    }
+
+    @Override
+    public int calculate(Cereal cereal) {
+        float cerealPrice = cereal.getPricePerKilo() * cereal.getWeight();
+        System.out.println(cereal.getName() + " price: " + cerealPrice);
+        return Math.round(cerealPrice);
+    }
+
+    @Override
+    public int calculate(Fridge fridge) {
+        System.out.println("Fridge " + fridge.getName() + " S/N: " + fridge.getSerialNumber() + " price: " + fridge.getValue());
+        if(fridge.getValue() > 600) {
+            System.out.println("Discount available!");
+            return fridge.getValue() - 50;
+        }
+        return fridge.getValue();
+    }
+}
